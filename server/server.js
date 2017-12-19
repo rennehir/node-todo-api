8ce4b161c7a1889d47257b72405666c22b82a45a
rennehir <git@rennehir.fi>
2017-12-19 16:55:34 +0200

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.post('/todos', (req, res) => {
   });
 
   todo.save().then((doc) => {
-    res.send(doc);
+    res.status(201).send(doc);
   }, (e) => {
     res.status(400).send(e);
   })
@@ -24,3 +24,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
