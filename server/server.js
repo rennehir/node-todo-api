126f69677a5783b9b89298a1049b28a7d0a8bd97
rennehir <git@rennehir.fi>
2017-12-19 16:12:03 +0200

    Setup server, db and models

---
+++ server/server.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user');
+
+let app = express();
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
