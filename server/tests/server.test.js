e9c76980811d272f502ba892541ad4040ad1c99c
rennehir <git@rennehir.fi>
2017-12-20 16:17:39 +0200

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -312,3 +312,25 @@ describe('POST /users/login', () => {
   });
 
 });
+
+describe('DELETE /users/me/token', () => {
+
+  it('should reomve auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          expect(user.tokens).toNotInclude(users[0].tokens[0].token);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+});
