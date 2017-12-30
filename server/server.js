96c41072759ef640de1570af94ebac61804f5522
rennehir <git@rennehir.fi>
2017-12-30 02:30:57 +0200

    Fix mongoose error with async/await calls

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
+let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
 let {authenticate} = require('./middleware/authenticate');
@@ -14,53 +15,48 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post('/todos', authenticate, async (req, res) => {
-  try {
-    const todo = new Todo({
-      text: req.body.text,
-      _creator: req.user._id
-    });
+app.post('/todos', authenticate, (req, res) => {
+  let todo = new Todo({
+    text: req.body.text,
+    _creator: req.user._id
+  });
 
-    await todo.save();
+  todo.save().then((doc) => {
     res.status(200).send(doc);
-  } catch (e) {
+  }).catch((e) => {
     res.status(400).send(e);
-  }
+  });
 });
 
-app.get('/todos', authenticate, async (req, res) => {
-  try {
-    const todo = await Todo.find({
-      _creator: req.user._id
-    });
-
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({todos});
-  } catch (e) {
+  }, (e) => {
     res.status(400).send(e);
-  }
+  });
 });
 
-app.get('/todos/:id', authenticate, async (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
   let id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  try {
-    const todo = await Todo.findOne({
-      _id: id,
-      _creator: req.user._id
-    });
-
+  Todo.findOne({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
 
     res.status(200).send({todo});
-  } catch (e) {
+  }).catch((e) => {
     res.status(400).send();
-  }
+  });
 });
 
 app.delete('/todos/:id', authenticate, async (req, res) => {
@@ -86,9 +82,9 @@ app.delete('/todos/:id', authenticate, async (req, res) => {
   }
 });
 
-app.patch('/todos/:id', authenticate, async (req, res) => {
-  const id = req.params.id;
-  const body = _.pick(req.body, ['text', 'completed']);
+app.patch('/todos/:id', authenticate, (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
@@ -101,24 +97,22 @@ app.patch('/todos/:id', authenticate, async (req, res) => {
     body.completedAt = null;
   }
 
-  try {
-    const todo = await Todo.findOneAndUpdate({
-      _id: id,
-      _creator: req.user._id
-    }, {
-      $set: body
-    }, {
-      new: true
-    });
-
+  Todo.findOneAndUpdate({
+    _id: id,
+    _creator: req.user._id
+  }, {
+    $set: body
+  }, {
+    new: true
+  }).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
 
     res.status(200).send({todo});
-  } catch (e) {
+  }).catch((e) => {
     res.status(400).send();
-  }
+  })
 });
 
 app.post('/users', async (req, res) => {
